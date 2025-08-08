0b71bf700913cf5ed6764b898c3ce6645cfa429a
ysjeondev <hpc9909@gmail.com>
2025-08-08 20:49:20 +0900

    book-137p까지 완료

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Home(){
+  return(
+    <div>
+        <h2>Home</h2>
+        Home...
+    </div>
+  );
+}
+
+function Topics(){
+  return(
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  )
+}
+
+function Contact(){
+  return(
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  )
+}
+
+function App(){
+  return (
+    <div>
+      <h1>Hello React Router DOM</h1>
+      <Home></Home>
+      <Topics></Topics>
+      <Contact></Contact>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
